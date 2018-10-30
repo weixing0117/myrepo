b5f24beceeaa6b040c77afdf0f77ed15b5babe76
Brined-Fish <19940117@qq.com>
2018-10-31 02:24:27 +0800

    upload bug

--- microservicecloud/microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
+++ microservicecloud/microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
@@ -25,6 +25,24 @@ public class Dept implements Serializable// entity --orm--- db_table
 		super();
 		this.dname = dname;
 	}
+	//import org.springframework.web.multipart.commons.CommonsMultipartResolver;//
 	
+	//MultipartFile file = ((MultipartRequest) request).getFile("upfile");
+	
+	// 显示声明CommonsMultipartResolver为mutipartResolver  
+/*    @Bean(name = "multipartResolver")  
+    public MultipartResolver multipartResolver()  
+    {  
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver();  
+        // resolver.setDefaultEncoding("UTF-8");  
+        // resolver.setResolveLazily(true);// resolveLazily属性启用是为了推迟文件解析，以在在UploadAction中捕获文件大小异常  
+        // resolver.setMaxInMemorySize(40960);  
+        resolver.setMaxUploadSize(10 * 1024 * 1024);// 上传文件大小 5M 5*1024*1024  
+        return resolver;  
+    }  
+    //启动类上添加
+    @EnableAutoConfiguration(exclude = {MultipartAutoConfiguration.class})  
+*/
+
 	
 }
